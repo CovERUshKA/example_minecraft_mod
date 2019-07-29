87c0b4cd4febe30ba7c2b191247ee61bf85775ef
CovERUshKA <43516399+CovERUshKA@users.noreply.github.com>
2019-07-29 11:05:29 +0300

    Update ModItems.java

--- src/main/java/ru/cover/test/init/ModItems.java
+++ src/main/java/ru/cover/test/init/ModItems.java
@@ -8,6 +8,4 @@ import ru.cover.test.items.ItemTestBlock;
 
 public class ModItems {
 	public static final List<Item> ITEMS = new ArrayList<Item>();
-	
-	//public static final Item TEST_BLOCK = new ItemTestBlock();
 }
